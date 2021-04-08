1a49d854aabcaf6a7eac161b760f51b76d51afde
Manuel <robinard.manuel@gmail.com>
2021-04-08 16:33:49 +0200

    added Geoloc custom Hook

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Home.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { icon, popup } from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Circle } from "react-leaflet";
 import L from "leaflet";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import LogoClub from "../../assets/LogoClub.png";
+import LogoLigue from "../../assets/logoLigue.png";
+import useGeolocation from '../Hook/useGeolocation';
 
 function Home() {
   const [instance, setInstance] = useState([]);
   const [club, setClub] = useState([]);
+  const location = useGeolocation();
+  const [countClose, setCountClose] = useState(0);
+  const [latMin, setLatMin] = useState(0)
+  const [latMax, setLatMax] = useState(0)
+  const [lngMin, setLngMin] = useState(0)
+  const [lngMax, setLngMax] = useState(0)
 
   const markerInstance = L.icon({
     iconSize: [60, 60],
@@ -18,10 +25,31 @@ function Home() {
   });
 
   const markerClub = L.icon({
-    iconSize: [60, 60],
+    iconSize: [50, 50],
     iconAnchor: [40, 60],
     iconUrl: LogoClub,
   });
+
+  const markerLigue = L.icon({
+    iconSize: [70, 70],
+    iconAnchor: [40, 60],
+    iconUrl: LogoLigue,
+  });
+
+
+  useEffect(() => {
+    if (location.loaded === true) {
+      setLatMin(location.coordinates.lat - 0.180227)
+      setLatMax(location.coordinates.lat + 0.180227)
+      setLngMin(location.coordinates.lng - 0.246349)
+      setLngMax(location.coordinates.lng + 0.246349)
+    } else {
+      setLatMin(0)
+      setLatMin(0)
+      setLngMin(0)
+      setLngMax(0)
+    }
+  }, [location])
 
   useEffect(() => {
     axios
@@ -40,20 +68,31 @@ function Home() {
   let setMap = [47.830261, 1.93609];
 
   return (
+    
     <MapContainer center={setMap} zoom={8} className="map">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+
       <Marker position={[47.830261, 1.93609]}>
-        <Popup>
+        <Popup className="LiguePopUp">
           <a href="https://stage.foot-centre.fr">
+            <img className="logoLigue" src={LogoLigue} />
             <h1>Ligue Centre Val de Loire </h1>
             <h2> {instance.info} </h2>
           </a>
         </Popup>
       </Marker>
 
+      {location.loaded === true ?
+          <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
+            <Popup>
+              <h2>{countClose} transactions ont eu lieu près de chez vous !</h2>
+            </Popup>
+            <Circle center={[location.coordinates.lat, location.coordinates.lng]} radius={20000} />
+          </Marker>
+          : null}
       {instance.map((instances) => {
         return (
           <Marker position={[instances.lat, instances.long]}>
@@ -64,7 +103,7 @@ function Home() {
           </Marker>
         );
       })}
-
+       
       {club.slice(0, 1000).map((clubs) => {
         return (
           <Marker position={[clubs.Lat, clubs.Long]} icon={markerClub}>
@@ -76,6 +115,7 @@ function Home() {
           </Marker>
         );
       })}
+     
     </MapContainer>
   );
 }
---
+++ src/components/Hook/useGeolocation.js
@@ -0,0 +1,40 @@
+import { useEffect, useState} from 'react';
+
+const useGelocation = () => {
+    const [location, setLocation] = useState({
+        loaded : false,
+        coordinates : {lat : "" , lng: ""},
+    })
+    
+    const onSucces = (location) => {
+        setLocation({
+            loaded: true, 
+            coordinates : {
+                lat: location.coords.latitude,
+                lng:location.coords.longitude,
+            },
+
+        });
+    }
+    const onError = error => {
+        setLocation({
+            loaded: false,
+            error,
+        });
+    }
+
+    useEffect(() => {
+        if(!("geolocation" in navigator)) {
+            onError({
+                code: 0,
+                message: "Geolocation not supported",
+            });
+        }
+
+        navigator.geolocation.getCurrentPosition(onSucces, onError);
+    }, []);
+
+    return location 
+}
+
+export default useGelocation
