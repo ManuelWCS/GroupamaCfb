b660d45db192289051baa53aa19bfed53a9f58f3
Manuel <robinard.manuel@gmail.com>
2021-06-02 14:35:48 +0200

    added new things before deploy

--- src/components/Club/Club.jsx
+++ src/components/Club/Club.jsx
@@ -3,10 +3,11 @@ import Header from "../header/Header.jsx";
 import "./Club.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { MapContainer, Marker, Popup, TileLayer, Circle } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import homebtn from "../../assets/Boutons/home.png";
 import { Link } from "react-router-dom";
-import buttonImg from '../../assets/button.png'
+import LogoClub from "../../assets/Marqueurs/SelectedClub.png";
+import L from "leaflet";
 
 function Club() {
   const [equipe, setEquipe] = useState([]);
@@ -15,6 +16,15 @@ function Club() {
   const [SelectedCategory, setSelectedCategory] = useState([]);
   const [club, setClub] = useState([]);
   const [clubChoisi, setClubChoisi] = useState([]);
+  const [position, setPosition] = useState([]);
+  const [clubLat, setclubLat] = useState([]);
+  const [clubLong, setclubLong] = useState([]);
+
+  const markerClub = L.icon({
+    iconSize: [50, 50],
+    iconAnchor: [23.5, 47],
+    iconUrl: LogoClub,
+  });
 
   useEffect(() => {
     axios
@@ -49,9 +59,13 @@ function Club() {
         (clubs) =>
           clubs.Localite === `${ville}` &&
           clubs.Category === `${SelectedCategory}`
+          
       )
     );
     console.log(clubChoisi);
+    console.log(SelectedCategory);
+    console.log(clubChoisi.Lat)
+    
   }
 
   return (
@@ -94,14 +108,13 @@ function Club() {
               })}
             </select>
           </form>
-          
         </div>
 
-        <div className="buttons"> 
+        <div className="buttons">
           <Link to="/">
             <img className="homebtn" src={homebtn} alt="acceuil" />
           </Link>
-          <button className="buttonClub" onClick={testFct}> 
+          <button className="buttonClub" onClick={testFct}>
             Test
           </button>
         </div>
@@ -111,60 +124,71 @@ function Club() {
         Les équipes près de <span className="red">{ville} </span>pour la
         <br></br> catégorie <span className="blue"> {SelectedCategory} </span>
       </h1>
-    <div className="resultsAndMap">
-
-      <div className="results">
-        {club
-          .filter(
-            (clubs) =>
-            clubs.Localite === `${ville}` &&
-            clubs.Category === `${SelectedCategory}`
+      <div className="resultsAndMap">
+        <div className="results">
+          {club
+            .filter(
+              (clubs) =>
+                clubs.Localite === `${ville}` &&
+                clubs.Category === `${SelectedCategory}`
             )
             .map((clubs, index) => (
               <div
-              className="resultsDiv"
-              className="resultsDivMobile"
-              key={index}
+                className="resultsDiv"
+                className="resultsDivMobile"
+                key={index}
               >
-              <h3 className="titleCard">Club : {clubs.NomChampionnat} </h3>
-              <br></br>
-              <h3>Equipe : {clubs.NomEquipe}</h3>
-              <h3>
-                {" "}
-                Ville du club : {clubs.Localite} {clubs.Lat} {clubs.Longitude}{" "}
-              </h3>
-              <h3> Adresse du club : {clubs.AdressePostale} </h3>
-              <h3>Nom de l'équipe : {clubs.NomEquipe} </h3>
-              <h3>
-                Mail et personne à contacter{clubs.MailClub} {clubs.Civilite}{" "}
-                {clubs.Nom} {clubs.Prenom}{" "}
-              </h3>{" "}
-              <h3>CATEGORIE {clubs.Category}</h3>{" "}
-            </div>
-          ))}
+                <h3 className="titleCard">Club : {clubs.NomChampionnat} </h3>
+                <br></br>
+                <h3>Equipe : {clubs.NomEquipe}</h3>
+                <h3>
+                  {" "}
+                  Ville du club : {clubs.Localite} {clubs.Lat} {clubs.Longitude}{" "}
+                </h3>
+                <h3> Adresse du club : {clubs.AdressePostale} </h3>
+                <h3>Nom de l'équipe : {clubs.NomEquipe} </h3>
+                <h3>
+                  Mail et personne à contacter{clubs.MailClub} {clubs.Civilite}{" "}
+                  {clubs.Nom} {clubs.Prenom}{" "}
+                </h3>{" "}
+                <h3>CATEGORIE {clubs.Category}</h3>{" "}
+              </div>
+            ))}
         </div>
-      <div className="Map">
-        <MapContainer
-          center={[47.830261, 1.93609]}
-          zoom={8}
-          className="leaflet-container2"
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={[47.830261, 1.93609]}>
-            <Popup className="LiguePopUp">
-              <a href="https://service-clubs.foot-centre.fr/">
-                <img className="logoLigue" />
-                <h1>Ligue Centre Val de Loire </h1>
-                <h2> </h2>
-              </a>
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
+        <div className="Map">
+          <MapContainer
+            center={[47.830261, 1.93609]}
+            zoom={6}
+            className="leaflet-container2"
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[47.830261, 1.93609]}>
+              <Popup className="LiguePopUp">
+                <a href="https://service-clubs.foot-centre.fr/">
+                  <img className="logoLigue" />
+                  <h1>Ligue Centre Val de Loire </h1>
+                  <h2> </h2>
+                </a>
+              </Popup>
+            </Marker>
 
+            {clubChoisi.map((clubsChoisis, index) => {
+              return (
+                <Marker
+                  position={[clubsChoisis.Lat, clubsChoisis.Longitude]}
+                  icon={markerClub}
+                >
+                  <Popup className="NameClub">
+                    <h1>{clubsChoisis.AdressePostale}</h1>
+                  </Popup>
+                </Marker>
+              );
+            })}
+          </MapContainer>
+        </div>
       </div>
     </div>
   );
