fe62109c077af51a8f0ba1f0a7535a03ad6d46ba
Manuel <robinard.manuel@gmail.com>
2021-11-15 12:25:14 +0100

    save before testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "./App.css";
 import { Switch, Route } from 'react-router';
 import Mobile from './components/Mobile/Mobile.jsx'
+import SmallMap from "./components/Testing/Test";
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
              <Switch>
             <Route exact path="/" component={Mobile}/> 
+            <Route path="/test" component={SmallMap}/>
        </Switch>
     </div>
   );
