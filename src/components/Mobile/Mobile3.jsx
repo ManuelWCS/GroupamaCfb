47424b46faf360efefaf8b20c8db976d8dd11530
= <=>
2021-11-22 17:09:29 +0100

    started styles

---
+++ src/components/Mobile/Mobile3.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../header/Header';
+import './Moible3.css';
+import svg from './Fond2.svg'
+
+function Mobile3() {
+    return (
+        <div className="fullPage">
+            <Header/>
+            {/* <img src={svg} className="background" /> */}
+            <div className="firstTitle">
+                <h1 className="titlePart1">Trouvez un club près </h1>
+                <h1 className="titlePart2">de chez vous ! </h1>
+
+                
+            
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Mobile3
