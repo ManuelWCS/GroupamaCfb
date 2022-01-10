52f0dac84c8ed454aae9f7bc4fdf141e66cc6ace
= <=>
2022-01-10 15:01:27 +0100

    centered new image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Switch, Route } from "react-router";
 import Testing from "./components/Map/Testing.jsx";
+import newHome from "./components/Home/Homepage.jsx"
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Testing} />
+        <Route path="/home" component={newHome}/>
       </Switch>
     </div>
   );
---
+++ src/components/Home/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Homepage.css";
+import Header from "../header/Header.jsx";
+import photo from "../../assets/img/accueil2.png";
+
+function Homepage() {
+  return (
+    <div>
+      <Header />
+      <div className="hero">
+        <img className="photo" />
+        <div className="content">
+          <h1 className="wow">BIENVENUE</h1>
+          <h2 className="wow2">TROUVEZ UN CLUB PRES DE CHEZ VOUS !</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
