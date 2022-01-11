44a1822aa45b52e658c8dd315600e30ddcb6515e
= <=>
2022-01-11 11:27:12 +0100

    added homepage

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import "./Homepage.css";
 import Header from "../header/Header.jsx";
-import photo from "../../assets/img/accueil2.png";
+import arrows from '../../assets/img/arrows.png'
 
 function Homepage() {
   return (
-    <div>
+    <div className="homepage">
       <Header />
-      <div className="hero">
+      <div className="mainContent">
         <img className="photo" />
-        <div className="content">
-          <h1 className="wow">BIENVENUE</h1>
-          <h2 className="wow2">TROUVEZ UN CLUB PRES DE CHEZ VOUS !</h2>
-          
-        </div>
-      </div>
-    </div>
+        <div className="contentWrapper">
+          <h1 className="mainTitle">BIENVENUE</h1>
+          <h3 className="mainDescription">TROUVEZ UN CLUB PRES DE CHEZ VOUS !</h3>
+        </div> {/* fin contentWrapper */}
+          <div className="arrowContainer">
+            <a href="https://relaxed-banach-2aac18.netlify.app/">
+
+            <img src={arrows} alt="" className="arrow" />
+            </a>
+
+          </div>
+      </div>{/* fin mainContent */}
+      <div className="imageWrapper">
+          {/* <img className="regionPicture" src={regionPicture} /> */}
+          </div> {/* fin imageWrapper */}
+
+          <div className="textContainer">
+            <h2 className="secondTitle"> 639 CLUBS DE FOOTBALL EN RÉGION CENTRE-VAL DE LOIRE</h2>
+            <h4 className="descriptionText">
+              La Ligue Centre-Val de Loire de Football et son partenaire Crédit Mutuel vous propose cette plateforme pour vous donner accès aux nombreux clubs de football implantés dans notre région
+            </h4>
+
+          </div> { /* Fin div textes*/}
+    </div> // fin homepage
   );
 }
 
