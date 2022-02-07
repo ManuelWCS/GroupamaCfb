460c9f7aaa59b536b0591833c274cc40ffff47c0
= <=>
2022-02-07 14:21:00 +0100

    footer

--- src/components/Clean/Clean.jsx
+++ src/components/Clean/Clean.jsx
@@ -52,11 +52,11 @@ import btnPicture from "../../assets/CompressedPictures/buttontransparent.webp";
 import Button2 from "@mui/material/Button";
 
 /* import du Footer */
-import fb from "../../assets/footer/fb.png";
-import web from "../../assets/footer/web.png";
-import yt from "../../assets/footer/youtube.png";
-import Logo from '../../assets/img/GroupamaLogo.png'
-import twi from '../../assets/img/twitter.png'
+import fb from "../../assets/CompressedPictures/fb.webp";
+import web from "../../assets/CompressedPictures/web.webp"
+import yt from "../../assets/CompressedPictures/youtube.webp"
+import Logo from "../../assets/CompressedPictures/GroupamaLogo.webp"
+import twi from '../../assets/CompressedPictures/twitter.webp'
 
 
 
@@ -888,6 +888,47 @@ function Clean() {
                 );
               })
               : null}
+          </div>
+
+          <div className="sponso">
+            <img src={Logo} className="GroupamaLogo"/>
+              <p className="sponsoText">  Cette plateforme est soutenue par Groupama</p>
+            <div className="sponsoLinks">
+            <a
+            href="https://www.facebook.com/groupama"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={fb} alt="" className="LinkGroupama" />
+          </a>
+
+          <a
+            href="https://www.youtube.com/channel/UCcVU-JnuCbANGOl4vGcGpEg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={yt} alt="" className="LinkGroupama" />
+          </a>
+
+          <a
+            href="https://www.groupama.fr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={web} alt="" className="LinkGroupama" />
+          </a>
+
+          <a
+            href="https://twitter.com/GroupeGroupama"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={twi} alt="" className="LinkGroupama" />
+          </a>
+
+            </div>
+
+            
           </div>
 
           <div className="footerv">
