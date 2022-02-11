908828d3f69826faea15ac000e5c178ceade11fc
= <=>
2022-02-11 15:36:36 +0100

    phone version

--- src/components/Clean/Clean.jsx
+++ src/components/Clean/Clean.jsx
@@ -920,48 +920,47 @@ function Clean() {
               </a>
             </div>
           </div>
-              <div className="foooooter">
-              <div className="logos">
-          <a
-            href="https://www.facebook.com/LCFofficiel/?ref=bookmarks"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className="logos" alt="logo Facebook" src={fb} />
-          </a>
-        </div>
+          <div className="foooooter">
+            <div className="logos">
+              <a
+                href="https://www.facebook.com/LCFofficiel/?ref=bookmarks"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="logos" alt="logo Facebook" src={fb} />
+              </a>
+            </div>
 
-        <div className="logos">
-          <a
-            href="https://foot-centre.fff.fr/wp-content/uploads/sites/9/prehome/prehome/index.html"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="logos"
-              alt="logo site Ligue Centre Val de Loire"
-              src={web}
-            />
-          </a>
-        </div>
+            <div className="logos">
+              <a
+                href="https://foot-centre.fff.fr/wp-content/uploads/sites/9/prehome/prehome/index.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="logos"
+                  alt="logo site Ligue Centre Val de Loire"
+                  src={web}
+                />
+              </a>
+            </div>
 
-        <div className="logos">
-          <a
-            href="https://www.youtube.com/channel/UCs6RtJ9tefoU0iRnTkNzD6Q"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="logos"
-              alt="logo Youtube Ligue Centre-Val de Loire"
-              src={yt}
-            />
-          </a>
-        </div>
-              </div>
+            <div className="logos">
+              <a
+                href="https://www.youtube.com/channel/UCs6RtJ9tefoU0iRnTkNzD6Q"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="logos"
+                  alt="logo Youtube Ligue Centre-Val de Loire"
+                  src={yt}
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-      
     </div>
   );
 }
