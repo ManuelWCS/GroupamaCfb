79b132c4888f677dc1f47bc455f9a61964789b90
= <=>
2022-05-11 17:15:43 +0200

    changes before go

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,42 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Description.css";
 
 function Description() {
   let limit = 541;
 
-
   const [counter, setCounter] = useState(0);
 
-  function increment(){
-    for(let i = 0; i < limit; i++){
-      if (counter < limit){
-      setTimeout(() => {
-        setCounter(counter + 10);
-
-      }, 1)}
-      else {
-        setCounter(limit)
+  function increment() {
+    for (let i = 0; i < limit; i++) {
+      if (counter < limit) {
+        setTimeout(() => {
+          setCounter(counter + 10);
+        }, 1);
+      } else {
+        setCounter(limit);
       }
     }
   }
-// function thaht make the counter stops at the limit
-  function stop(){
+  // function thaht make the counter stops at the limit
+  function stop() {
     setCounter(limit);
   }
 
-
   useEffect(() => {
     increment();
-  }, [counter])
+  }, [counter]);
   return (
     <div className="descriptionContainer">
       <h2 className="mainDescription">
-        {counter} CLUBS DE FOOTBALL EN RÉGION CENTRE-VAL DE LOIRE
+        541 CLUBS DE FOOTBALL EN RÉGION CENTRE-VAL DE LOIRE
       </h2>
       <h4 className="secondaryDescription">
         La Ligue Centre-Val de Loire de Football vous propose cette plateforme
         afin de découvrir l'ensemble des clubs de notre Région !
-        
       </h4>
     </div>
   );
--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -31,32 +31,32 @@ function Preloader(props) {
   }, [counter]);
 
   return (
-  <>
-    <div className="FullLanding">
-      <div className="headerPreload">
-        <Link to="">
-          <img src={ecussonLigue} className="headerLogo" alt="logo" />
-        </Link>
-      </div>
-      <div className="LandingTitlesContainer">
-        <div className="BiggestTitle">
-          <div className="firstLine">
-            <img src={background} className="background" alt="background" />
-            <h1 className="counterNumber">{counter}</h1>
-            <h2 className="LandingTitle">CLUBS</h2>
-          </div>
-          <div className="secondLine">
-            <h2 className="LandingTitle2">DE FOOTBALL</h2>
-          </div>
-          <div className="thirdLine">
-            <h4 className="LandingTitle3"> EN RÉGION CENTRE VAL DE LOIRE </h4>
-          </div>
-          <h4 className="LandingTitle4"> TROUVEZ UN CLUB PRÈS DE CHEZ VOUS </h4>
+    <>
+      <div className="FullLanding">
+        <div className="headerPreload">
+          <Link to="">
+            <img src={ecussonLigue} className="headerLogo" alt="logo" />
+          </Link>
         </div>
-       
-      </div>
-     
-    <div className="LinkContainer">
+        <div className="LandingTitlesContainer">
+          <div className="BiggestTitle">
+            <div className="firstLine">
+              <img src={background} className="background" alt="background" />
+              <h1 className="counterNumber">{counter}</h1>
+              <h2 className="LandingTitle">CLUBS</h2>
+            </div>
+            <div className="secondLine">
+              <h2 className="LandingTitle2">DE FOOTBALL</h2>
+            </div>
+            <div className="thirdLine">
+              <h4 className="LandingTitle3"> EN RÉGION CENTRE VAL DE LOIRE </h4>
+            </div>
+            <h4 className="LandingTitle4"> TROUVEZ UN CLUB PRÈS DE CHEZ VOUS </h4>
+          </div>
+
+        </div>
+
+        <div className="LinkContainer">
           <Link to="/trouvetonclub">
             <img
               src={LogoCFB}
@@ -70,7 +70,7 @@ function Preloader(props) {
 
         </div>
         <h4 className="LandingTitle5"> Plateforme dévéloppée par la Ligue du Centre-Val de Loire de Football </h4>
-    </div>
+      </div>
     </>
   );
 }
