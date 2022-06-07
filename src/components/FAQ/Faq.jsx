235043e111d4cac7534968d2a2c68bd684a14086
= <=>
2022-06-07 14:31:02 +0200

    disposition OK

--- src/components/FAQ/Faq.jsx
+++ src/components/FAQ/Faq.jsx
@@ -10,10 +10,12 @@ function Faq() {
     <div className="faqContainer">
     <h6 className="faqTitle">FAQ ! BESOIN D'AIDE ? </h6>
     <p className="faqText2">Vous avez une question ? Nous avons probablement la réponse !</p>
-    <Link to="/faq">
-
+    <a href="mailto:name@email.com">
     <img src={mailPicture} className="contactImg" alt="courrier électronique" />
-    </Link>
+    </a>
+ 
+
+
   </div>
   )
 }
