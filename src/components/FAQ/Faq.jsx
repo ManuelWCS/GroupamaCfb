be0daf197c688594415aca2dbe9a34c39f325c25
= <=>
2022-06-08 16:35:19 +0200

    faq mail done

--- src/components/FAQ/Faq.jsx
+++ src/components/FAQ/Faq.jsx
@@ -10,7 +10,7 @@ function Faq() {
     <div className="faqContainer">
     <h6 className="faqTitle">FAQ ! BESOIN D'AIDE ? </h6>
     <p className="faqText2">Vous avez une question ? Nous avons probablement la réponse !</p>
-    <a href="mailto:name@email.com">
+    <a href="mailto:mrobinard@centre.fff.fr?subject=Vous rencontrez un bug, faites moi part du problème !">
     <img src={mailPicture} className="contactImg" alt="courrier électronique" />
     </a>
  
