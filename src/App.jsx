171d2424c2bf11c03367e2f0e6189a56b7ef8825
= <=>
2022-06-08 17:16:29 +0200

    cfb final mardi

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         declineButton="En savoir plus"
         ><p className="cookieTitle">La Ligue Centre-Val de Loire respecte votre vie privée !</p>
         <p className="cookieDescription"> 
-        Avec votre accord, nos partenaires et nous utilisons des cookies ou technologies similaires pour stocker et accéder à des informations personnelles comme votre visite sur ce site. Vous pouvez retirer votre consentement ou vous opposer aux traitements basés sur l'intérêt légitime à tout moment en cliquant sur "En savoir plus" ou dans notre politique de confidentialité sur <a href="https://www.fff.fr/456-protection-des-donnees.html">ce site. </a></p>
+        Avec votre accord, nos partenaires et nous utilisons des cookies ou technologies similaires pour stocker et accéder à des informations personnelles comme votre visite sur ce site. Vous pouvez retirer votre consentement ou vous opposer aux traitements basés sur l'intérêt légitime à tout moment en cliquant  <a href="https://www.fff.fr/456-protection-des-donnees.html">ici </a> ou dans notre politique de confidentialité sur le site de la Ligue du Centre-Val de Loire de Football</p>
         </CookieConsent>
       </div>
     </>
