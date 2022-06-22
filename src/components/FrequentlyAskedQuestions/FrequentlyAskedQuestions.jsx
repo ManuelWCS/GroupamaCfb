09ece26cdc40da09ba8b72fb015be4ae2a0c9392
= <=>
2022-06-22 17:31:37 +0200

    added mobile FAQ

--- src/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
+++ src/components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
@@ -1,20 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./FrequentlyAskedQuestions.css";
-import Rubrique from './Rubriques.jsx';
-import {faqs} from './faqs.js';
+import AccordionItem from "./AccordionItem.jsx";
+import { faqs } from "./faqs.js";
+import { faqMap } from "./faqMap.js";
+import LogoCFB from "../../assets/CompressedPictures/Logos/logoplateforme.webp";
 
 function FrequentlyAskedQuestions() {
-    return (
-        <main className='containerFAQ'>
+  const [clicked, setClicked] = useState("0");
 
-            {faqs.map((faq, index) => {
-                return <Rubrique key={index} faq={faq} />
-                }
-                )}
-                
-        </main>
+  const handleToggle = (index) => {
+    if (clicked === index) {
+      return setClicked("0");
+    }
+    setClicked(index);
+  };
 
-  )
+  const [selected, setSelected] = useState("1");
+
+
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+    console.log(selected, "select");
+  }
+
+  return (
+    <main className="containerFAQ">
+      <div className="headerPreload">
+        <img
+          src={LogoCFB}
+          alt="logo Centre Frappe & But"
+          className="LogoPlateforme"
+        ></img>
+      </div>
+      
+      <div className="chooseContainer">
+        <button 
+        onClick={handleSelect} 
+        value="1" 
+        className="chooseBtn"> Naviguer sur CFB </button>
+        <button 
+        onClick={handleSelect}
+        value="2"
+        className="chooseBtn">La carte</button>
+      </div>
+      {selected ? ( 
+      <span>{selected === "1" ? "Naviguer sur CFB " : "carte" }</span> ) : null }
+
+
+      <div className="rowToColumnContainer">
+
+        {selected === "1" ? (
+      <ul className="accordion">
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem
+              key={index}
+              faq={faq}
+              onToggle={() => handleToggle(index)}
+              active={clicked === index}
+            />
+          );
+        })}
+      </ul> ) : (
+      <ul className="accordion">
+        {faqMap.map((faq, index) => {
+          return (
+            <AccordionItem
+              key={index}
+              faq={faq}
+              onToggle={() => handleToggle(index)}
+              active={clicked === index}
+            />
+          );
+        })}
+      </ul> ) }
+
+      </div>
+      
+    </main>
+  );
 }
 
-export default FrequentlyAskedQuestions
+export default FrequentlyAskedQuestions;
---
+++ src/components/FrequentlyAskedQuestions/faqMap.js
@@ -0,0 +1,26 @@
+export const faqMap = [{
+    question: "Que signifie sur la carte le marqueur Ma position ? ",
+    answer: "je suis pas sur que j'ai besoin de répondre",
+},
+{
+    question: " Que signifie sur la carte le marqueur Club de football ?",
+    answer: "Cela signifie qu'il existe une discothèque appelé '2 football' à cet endroit.",
+},
+{
+    question: "Que signifie sur la carte le marqueur Club de football ?",
+    answer: "Roh concentre toi la",
+},
+{
+    question: "Que signifie sur la carte le marqueur Partenaire ?",
+    answer: "Cela signifie qu'il existe un partenaire à cet endroit.",
+},
+{
+    question: "Que signifie sur la carte les marqueurs sur la carte qui n'aparaissent pas dans la légende  ?",
+    answer: "quelles ne sont pas dans la légende, next ? ",
+},
+{
+    question: "Comment se déplacer sur la carte ?",
+    answer: "avec tes deux pieds mon gars",
+},
+
+];
