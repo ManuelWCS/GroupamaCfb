448719e0caaa5b3e90b5c193058cd6617335412a
= <=>
2022-06-23 17:17:59 +0200

    started component

--- src/components/FrequentlyAskedQuestions/faqMap.js
+++ src/components/FrequentlyAskedQuestions/faqMap.js
@@ -1,26 +1,18 @@
 export const faqMap = [{
     question: "Que signifie sur la carte le marqueur Ma position ? ",
-    answer: "je suis pas sur que j'ai besoin de répondre",
+    answer: "Ce marqueur indique votre position actuelle, il est disponible si vous autorisez la géolocalisation",
 },
 {
     question: " Que signifie sur la carte le marqueur Club de football ?",
-    answer: "Cela signifie qu'il existe une discothèque appelé '2 football' à cet endroit.",
+    answer: "Ce marqueur indique un club de football, il est disponible sans la géolocalisation",
 },
-{
-    question: "Que signifie sur la carte le marqueur Club de football ?",
-    answer: "Roh concentre toi la",
-},
-{
-    question: "Que signifie sur la carte le marqueur Partenaire ?",
-    answer: "Cela signifie qu'il existe un partenaire à cet endroit.",
-},
+
 {
     question: "Que signifie sur la carte les marqueurs sur la carte qui n'aparaissent pas dans la légende  ?",
-    answer: "quelles ne sont pas dans la légende, next ? ",
+    answer: "Ces marqueurs représentent les institutions fédérales, plusieurs districts et une seule ligue dans la région",
 },
 {
     question: "Comment se déplacer sur la carte ?",
-    answer: "avec tes deux pieds mon gars",
+    answer: "Cliquez sur la carte pour déplacer votre position, vous pouvez aussi utiliser les touches fléchées pour déplacer votre position",
 },
-
 ];
--- src/components/FrequentlyAskedQuestions/faqs.js
+++ src/components/FrequentlyAskedQuestions/faqs.js
@@ -1,10 +1,10 @@
 export const faqs = [{
         question: "A quoi sert la plateforme Centre, Frappe et But ? ",
-        answer: "Concrètement je sais pas mon reuf. Mais je crois que c'est une bonne idée.",
+        answer: "La plateforme Centre Frappe et But permet de vous localiser pour vous proposer un club près de chez vous, il est aussi possible de faire une recherche pour voir tous les clubs de la région ",
     },
     {
         question: "Comment trouver un club près de chez moi ? ",
-        answer: "Vous pouvez utiliser la recherche sur la page d'accueil. Vous pouvez aussi utiliser la recherche sur la page de recherche.",
+        answer: "Cliquez sur le bouton rouge et regardez la liste des clubs sur votre gauche, ils sont trié par ordre de distance ",
     },
     {
         question: "La plateforme est-elle disponible sur plusieurs supports ? ",
@@ -12,10 +12,10 @@ export const faqs = [{
     },
     {
         question: "Comment contacter la LCVLF ou son partenaire (...) ",
-        answer: "appelle chacal",
+        answer: "Vous pouvez nous contacter via le mail dans la rubrique contact, ou directement sur le site de la Ligue du Centre-Val de Loire",
     },
     {
         question: "Je rencontre un problème de naviagation, je ne trouve pas de réponse à ma question",
-        answer: "ah. dommage bro",
+        answer: "Merci de nous faire remonter tout soucis ou mauvaise expérience que vous rencontrez, nous sommes soucieux de vous garantir un service de qualité en améliorant ce qui ne va pas.",
     },
 ];
