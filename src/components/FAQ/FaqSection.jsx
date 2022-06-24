18bb5002459261ec55daba27125b68cb23f862c4
= <=>
2022-06-24 14:57:20 +0200

    added faq

--- src/components/FAQ/FaqSection.jsx
+++ src/components/FAQ/FaqSection.jsx
@@ -4,7 +4,7 @@ import './FaqSection.css'
 function FaqSection() {
   return (<div className='faqSection'>
 
-  <h1 className='faqTitle'>FAQ ! BESOIN D'AIDE ? </h1>
+  <h1 className='faqSectionTitle'>FAQ ! BESOIN D'AIDE ? </h1>
   <h5 className='faqSectionDescription'> Bienvenue sur la FAQ de la plateforme Centre, Frappe et But !</h5>
 
   <h6 className='faqSectionDescription2'>Vous avez une question ? Nous avons probalement la réponse</h6>
